6c4d27f5c749b9c87ebe5d5464b0db4240d50ac2
Ruben E Acosta <eldragondera1@gmail.com>
2019-12-28 13:13:52 -0500

    first plugin

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,11 @@
 import MediaPlayer from './mediaPlayer.js'
+import AutoPlay from './plugins/autoplay.js'
 
 const video = document.querySelector('video')
-const player = new MediaPlayer(video)
+const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()] })
 
-const button = document.querySelector('button')
+const button = document.querySelector('#toggle')
+const buttonMute = document.querySelector('#mute')
+
 button.onclick = () => player.togglePlay()
+buttonMute.onclick = () => player.toggleMute()
--- assets/mediaPlayer.js
+++ assets/mediaPlayer.js
@@ -1,12 +1,45 @@
-function MediaPlayer(el) {
-  this.media = el
+function MediaPlayer(config) {
+  this.media = config.el
+  this.plugins = config.plugins || []
+
+  this._initPlugins()
+}
+
+MediaPlayer.prototype._initPlugins = function() {
+  this.plugins.forEach(plugin => {
+    plugin.run(this)
+  })
+}
+
+MediaPlayer.prototype.play = function() {
+  this.media.play();
+}
+
+MediaPlayer.prototype.pause = function() {
+  this.media.pause();
+}
+
+MediaPlayer.prototype.mute = function() {
+  this.media.muted = true
+}
+
+MediaPlayer.prototype.unmute = function() {
+  this.media.muted = false
 }
 
 MediaPlayer.prototype.togglePlay = function() {
   if (this.media.paused) {
-    this.media.play();
+    this.play();
   } else {
-    this.media.pause()
+    this.pause()
+  }
+}
+
+MediaPlayer.prototype.toggleMute = function() {
+  if (this.media.muted) {
+    this.unmute();
+  } else {
+    this.mute()
   }
 }
 
