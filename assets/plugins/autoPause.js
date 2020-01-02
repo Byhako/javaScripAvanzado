a5560932a11ac06e9c1cce724c5259297719001f
Ruben E Acosta <eldragondera1@gmail.com>
2020-01-02 12:46:24 -0500

    VisibilityChange

--- assets/plugins/autoPause.js
+++ assets/plugins/autoPause.js
@@ -2,6 +2,7 @@ class AutoPause {
   constructor() {
     this.threshold = 0.25
     this.handlerIntersection = this.handlerIntersection.bind(this)
+    this.handleVisibilityChange = this.handleVisibilityChange.bind(this)
   }
   run(player) {
     this.player = player
@@ -12,6 +13,7 @@ class AutoPause {
       }
     )
     observer.observe(this.player.media)
+    document.addEventListener("visibilitychange", this.handleVisibilityChange)
   }
 
   handlerIntersection(entries) {
@@ -19,6 +21,16 @@ class AutoPause {
     console.log(entry)
 
     if (entry.intersectionRatio >= this.threshold) {
+      this.player.play()
+    } else {
+      this.player.pause()
+    }
+  }
+
+  handleVisibilityChange() {
+    const isVisible = document.visibilityState === 'visible'
+
+    if (isVisible) {
       this.player.play()
     } else {
       this.player.pause()
